e1557f07e52469087ed49762bd922ecab16382bf
Cuadra13 <nelsoncuadra844@gmail.com>
2022-10-29 16:02:43 -0600

    commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
+app.get('/', function(req,res){
+res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT. function(req, res) => {
+    console.log("El puerto esta corriendo el puerto TCP" + process.env.PORT)
+})
